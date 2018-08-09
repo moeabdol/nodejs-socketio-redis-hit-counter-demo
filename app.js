1b539512859c63e9289dd7f981e0d0a381569ff7
Mohammad Abdullah Saed <mohdsaed@hungerstation.com>
2018-08-09 17:41:58 +0300

    Finish implementing the hit counter

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express  = require('express');
 const http     = require('http');
 const socketIO = require('socket.io');
 const path     = require('path');
+const redis    = require('redis');
 
 const app    = express();
 const server = http.createServer(app);
 const io     = socketIO(server);
+const client = redis.createClient();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
@@ -15,6 +17,14 @@ server.listen(3000, err => {
   console.log('server listening on port 3000');
 });
 
-io.on('connect', () => {
+io.on('connect', socket => {
   console.log('client connected');
+
+  socket.on('hello', data => {
+    socket.join(data.hashID);
+
+    client.incr(data.hashID, (err, count) => {
+      io.to(data.hashID).emit('stats', count);
+    });
+  });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,25 @@
-var arr1 = [1, 2, 3, 4, 5];
-var arr2 = [6, 7, 8, 9, 10];
+$(document).ready(function() {
+  var arr1 = [1, 2, 3, 4, 5];
+  var arr2 = [6, 7, 8, 9, 10];
 
-function addArrays() {
-  return arr1 + ',' + arr2;
-}
+  function addArrays() {
+    return arr1 + ',' + arr2;
+  }
 
-console.log(addArrays());
+  console.log(addArrays());
+
+  var socket = io('http://localhost:3000');
+
+  socket.on('connect', function() {
+    console.log('connected to server');
+
+    if (window.location.hash) {
+      const hashID = window.location.hash.substring(1);
+      socket.emit('hello', { hashID: hashID });
+    }
+  });
+
+  socket.on('stats', hits => {
+    document.getElementById('hitCount').innerText = hits;
+  });
+});
