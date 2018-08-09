c039692455699b3ee1dfbd397e44c414677bef61
Mohammad Abdullah Saed <mohdsaed@hungerstation.com>
2018-08-09 16:35:24 +0300

    Add public folder

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const app    = express();
 const server = http.createServer(app);
 const io     = socketIO(server);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 server.listen(3000, err => {
   if (err) return console.log(err);
   console.log('server listening on port 3000');
+});
+
+io.on('connect', () => {
+  console.log('client connected');
 });
